9fd1595c96c51fb62bdd251a25f6e54f3c0b4855
Ricardo Freitas <derfreitas@gmail.com>
2019-03-03 17:56:06 +0000

    added some description parsing tests

--- weigh_in_test.py
+++ weigh_in_test.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from nose.tools import *
 
 import weigh_in
@@ -17,3 +19,19 @@ def test_inverses():
         back_cur_size, back_prev_size = weigh_in.parse_description(desc)
         assert back_cur_size == current_size, (desc, back_cur_size)
         assert back_prev_size == prev_size, (desc, back_prev_size)
+
+def test_parse_description():
+    status = "451,254 bytes"
+    back_cur_size, back_prev_size = weigh_in.parse_description(status)
+    assert back_cur_size == 451254
+    assert back_prev_size == None
+
+    status = "+11,425 bytes (+2.53%) → 451,254 bytes"
+    back_cur_size, back_prev_size = weigh_in.parse_description(status)
+    assert back_cur_size == 451254
+    assert back_prev_size == 451254-11425
+
+    status = "Reached limit of 2%: +11,425 bytes (+2.53%) → 451,254 bytes"
+    back_cur_size, back_prev_size = weigh_in.parse_description(status)
+    assert back_cur_size == 451254
+    assert back_prev_size == 451254-11425
